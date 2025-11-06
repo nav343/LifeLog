db84e2751928fb28fa0a9008b4f611e87152a89d
nav343 <94112427+nav343@users.noreply.github.com>
2025-11-06 18:43:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,524 @@
+from utils.window import Window
+import pickle
+import time
+import os
+from datetime import datetime
+
+moods = {
+    1: "😊 Happy",
+    2: "😌 Calm",
+    3: "😔 Sad",
+    4: "😤 Stressed",
+    5: "😎 Confident",
+}
+logo = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣧⠀⠀⣴⠄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⡀⣰⣏⡤⠀⢀⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢳⣦⣄⡘⢿⣿⣇⢏⣱⠖⣠⣿⠁⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣦⠹⡈⡎⢫⣾⣿⡟⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠳⡝⣿⣧⠹⠀⣾⡿⠟⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⣀⣀⡙⣖⠻⡞⡔⢰⢩⡷⣻⡁⢀⣤⣶⠖⠀
+⠀⠀⠀⠀⣠⣾⡿⠟⠛⠻⢷⡛⠘⢸⠃⠊⠿⢁⣾⣿⡿⠃⠀⠀
+⠀⠀⢠⣾⠟⠃⠀⠀⠀⠀⠀⠹⡀⠈⢀⣠⣴⡿⠟⠋⢀⣀⣤⠤
+⠀⠀⠀⠀⢀⣠⣤⣤⣄⠀⠀⠀⢀⠗⠁⠀⣠⣴⣶⣿⣿⡿⠋⠀
+⠀⠀⣠⣾⣿⣿⣿⣿⠟⠃⠢⡄⡜⠀⢠⣾⣿⣿⡿⠟⠋⠀⠀⠀
+⠒⠛⠛⠻⠟⠿⠛⠁⠀⠀⠀⢈⢋⠶⡛⠋⠉⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠖⠁⢠⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡈⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀
+LifeLog
+"""
+
+def Header(msg: str):
+    screen.print("═"*(os.get_terminal_size().columns - 6))
+    screen.print(msg, True)
+    screen.print("═"*(os.get_terminal_size().columns - 10), True)
+    screen.print()
+
+def Login():
+    users = []
+    data = []
+    if os.path.exists(".lifelog/user.dat"):
+        with open(".lifelog/user.dat", 'rb') as F:
+            while True:
+                try:
+                    element = pickle.load(F)
+                    data.append(element)
+                    users.append(element['username'])
+                except EOFError:
+                    break
+    Header("🌿 LOGIN 🌿")
+    screen.render()
+    username = screen.input("Username (or q to quit): ")
+    if username == 'q':
+        screen.quit()
+    if username not in users:
+        screen.print(f"User not found. It seems like you have not created an account. Try creating it")
+        screen.render()
+        screen.quit()
+
+    password = screen.input("Password: ", True)
+    user = {}
+    for u in data:
+        if u['username'] == username:
+            user = u
+
+    if user['username'] == username and user['password'] != password:
+            screen.print(f"Incorrent Password. Try again")
+            screen.render()
+            time.sleep(1)
+            screen.clear()
+            Login()
+    else:
+        stats = loadStats()
+        stats['logged'] = True
+        with open(".lifelog/stats.dat", 'wb') as file:
+            pickle.dump(stats, file)
+        screen.print()
+        screen.print(f"[✔] Login successful! Welcome back, {username} !!")
+        screen.print("Loading your dashboard...")
+        screen.render()
+        time.sleep(1)
+        screen.clear()
+        screen.render()
+        Dashboard(username)
+
+def ViewAll(username: str):
+    Header("📖  VIEW ALL ENTRIES  📖")
+    screen.print("All of Diary entries: ")
+    screen.print()
+    idx = 1
+    for i in os.listdir(f".lifelog/{username}"):
+        with open(f".lifelog/{username}/{i}", 'rb') as file:
+            data = pickle.load(file)
+            mood = moods[int(data['mood'])]
+            date = data['date']
+            content = data['content'][:30] + "..."
+            screen.print(f"[{idx}] [{date}] - [{mood} ]")
+            screen.print(f"Title: {i.replace("_", " ").title()[:-4]}")
+            screen.print("-"*(os.get_terminal_size().columns - 4))
+            screen.print(content)
+            screen.print("-"*(os.get_terminal_size().columns - 4))
+        idx += 1
+        screen.print()
+        screen.print()
+
+    choice = int(screen.input("Which entry do you want to see? (-1 to quit): "))
+    if choice > len(os.listdir(f".lifelog/{username}")):
+        screen.print("Invalid. Try again")
+        screen.render()
+    else:
+        if choice == -1:
+            screen.clear()
+            screen.render()
+        else:
+            screen.clear()
+            screen.render()
+            with open(f".lifelog/{username}/{os.listdir(f".lifelog/{username}")[choice-1]}", 'rb') as file:
+                data = pickle.load(file)
+                mood = moods[int(data['mood'])]
+                date = data['date']
+                content = data['content']
+                screen.print(f"[{date}] - [{mood} ]")
+                screen.print(f"Title: {os.listdir(f".lifelog/{username}")[choice-1].replace("_", " ").title()[:-4]}")
+                screen.print("-"*(os.get_terminal_size().columns - 4))
+                screen.print(content)
+                screen.print("-"*(os.get_terminal_size().columns - 4))
+            screen.print()
+            screen.input("Press ENTER to go back!")
+
+def ViewDateRange(username: str):
+    Header("📖  VIEW ENTRIES BY DATE 📖")
+    screen.print("Choose how you’d like to view your entries:")
+    screen.print()
+    start = datetime.strptime(screen.input("Enter Start Date: (DD-MM-YYYY) "), '%d-%m-%Y')
+    end = datetime.strptime(screen.input("Enter End Date: (DD-MM-YYYY) "), '%d-%m-%Y')
+    screen.print()
+    idx = 1
+    for i in os.listdir(f".lifelog/{username}"):
+        with open(f".lifelog/{username}/{i}", 'rb') as file:
+            data = pickle.load(file)
+            mood = moods[int(data['mood'])]
+            date = datetime.strptime(datetime.strptime(data['date'], "%d %B %Y").strftime("%d-%m-%Y"), "%d-%m-%Y")
+            content = data['content'][:30] + "..."
+            if date > start and date < end:
+                screen.print(f"[{idx}] [{date.strftime("%d %b %Y")}] - [{mood} ]")
+                screen.print(f"Title: {i.replace("_", " ").title()[:-4]}")
+                screen.print("-"*(os.get_terminal_size().columns - 4))
+                screen.print(content)
+                screen.print("-"*(os.get_terminal_size().columns - 4))
+        idx += 1
+        screen.print()
+        screen.print()
+        screen.render()
+    screen.input("Press ENTER to go back!!")
+
+def ViewByMood(username: str):
+    Header("💭 VIEW ENTRIES BY MOOD 💭")
+    screen.print("Which mood would you like to explore?")
+    screen.print()
+    screen.print("""[1] 😊 Happy
+[2] 😌 Calm
+[3] 😔 Sad
+[4] 😤 Stressed
+[5] 😎 Confident
+[6] Back to menu
+    """)
+    mood = int(screen.input("Enter your mood: "))
+    if mood == 6:
+        screen.clear()
+        screen.render()
+    else:
+        mood = moods[mood]
+        idx = 1
+        screen.clear()
+        screen.render()
+        Header(f"{mood[0]} ENTRIES WITH MOOD [{mood[1:]}] {mood[0]}")
+        for i in os.listdir(f".lifelog/{username}"):
+            with open(f".lifelog/{username}/{i}", 'rb') as file:
+                data = pickle.load(file)
+                dMood = moods[int(data['mood'])]
+                date = data['date']
+                content = data['content'][:30] + "..."
+                if mood == dMood:
+                    screen.print(f"[{idx}] [{date}] - Title: {i.replace("_", " ").title()[:-4]}")
+                    screen.print("-"*(os.get_terminal_size().columns - 4))
+                    screen.print(content)
+                    screen.print("-"*(os.get_terminal_size().columns - 4))
+            idx += 1
+            screen.print()
+            screen.print()
+            screen.render()
+        screen.input("Press ENTER to go back!!")
+
+def ViewEntries(username: str):
+    Header("📖  VIEW PAST ENTRIES  📖")
+    screen.print("Choose how you’d like to view your entries:")
+    screen.print()
+    screen.print("""[1] View all entries  
+[2] View by date range  
+[3] View by mood  
+[4] Back to dashboard""")
+    screen.print("-"*(os.get_terminal_size().columns - 4))
+    choice = int(screen.input("Enter your choice (1-4): "))
+    screen.clear()
+    screen.render()
+    if choice == 1:
+        ViewAll(username)
+
+    elif choice == 2:
+        ViewDateRange(username)
+
+    elif choice == 3:
+        ViewByMood(username)
+
+    elif choice == 4:
+        screen.clear()
+        screen.render()
+    else:
+        screen.clear()
+        screen.render()
+
+def MoodAnalytics(username: str):
+    Header("📊  MOOD ANALYTICS  📊")
+    screen.print("Your Mood record:")
+    screen.print()
+    stats = {1:0,2:0,3:0,4:0,5:0}
+    for i in os.listdir(f".lifelog/{username}"):
+        with open(f".lifelog/{username}/{i}", 'rb') as file:
+            data = pickle.load(file)
+            mood = int(data['mood'])
+            stats[mood] += 1
+    screen.print(f"😊 Happy     →  {stats[1]} days")
+    screen.print(f"😌 Calm      →  {stats[2]} days")
+    screen.print(f"😔 Sad       →  {stats[3]} days")
+    screen.print(f"😤 Stressed  →  {stats[4]} days")
+    screen.print(f"😎 Confident →  {stats[5]} days")
+    screen.print()
+    if stats != {1:0,2:0,3:0,4:0,5:0}:
+        max = 0
+        mostCommon = 1
+        for i in stats.items():
+            if i[1] > max: 
+                mostCommon = i[0]
+                i = max
+        screen.print()
+        screen.print('-'*(os.get_terminal_size().columns-4))
+        screen.print(f"Most common mood: {moods[mostCommon]}")
+        screen.render()
+    screen.input("Press ENTER to go back!!")
+    screen.clear()
+    screen.render()
+
+def EditDelete(username: str):
+    Header("🧹  EDIT / DELETE ENTRIES  🧹")
+    screen.print("Here are your recent entries:")
+    idx = 1
+    titles = []
+    for i in os.listdir(f".lifelog/{username}"):
+        with open(f".lifelog/{username}/{i}", 'rb') as file:
+            data = pickle.load(file)
+            mood = moods[int(data['mood'])][2:]
+            titles.append(i.replace("_", " ").title()[:-4])
+            date = datetime.strptime(data['date'], "%d %B %Y").strftime("%d %b %Y")
+            screen.print(f"[{idx}] [{date}] - {i.replace("_", " ").title()[:-4]} ({mood})")
+        idx += 1
+    screen.print('-'*(os.get_terminal_size().columns-4))
+
+    choice = int(screen.input("Index of the entry to edit/delete:"))-1
+    if choice < 0 or choice > idx:
+        screen.clear()
+        screen.render()
+    else:
+        screen.clear()
+        screen.render()
+        screen.print(f"You selected: {titles[choice]}")
+        screen.print()
+        screen.print("What would you like to do?")
+        screen.print("[1] Edit this entry")
+        screen.print("[2] Delete this entry")
+        screen.print("[3] Cancel")
+        screen.print('-'*(os.get_terminal_size().columns-4))
+        action = int(screen.input("Enter your choice: "))
+        if action == 1:
+            screen.clear()
+            screen.render()
+            screen.print("Editing Entry...")
+            newMood = screen.input("New Mood: ")
+            screen.print("Modify text below (type END when done):")
+            diary = screen.editor("> ") 
+            screen.print()
+            screen.print("-"*(os.get_terminal_size().columns - 4))
+            screen.print()
+            with open(f".lifelog/{username}/{titles[choice].replace(" ","_").lower()}.dat", 'wb') as file:
+                data = { "content": diary, "mood": newMood, "date": datetime.now().strftime("%d %B %Y") }
+                pickle.dump(data, file)
+            screen.print("[✔] Entry updated successfully!")
+            screen.render()
+
+        elif action == 2:
+            confirm = screen.input("Are you sure you want to delete this entry? (Y/N)")
+            if confirm.upper() == 'Y':
+                os.remove(f'.lifelog/{username}/{titles[choice].replace(" ", "_").lower()}.dat')
+        else:
+            screen.clear()
+            screen.render()
+    
+
+def Dashboard(username: str):
+    lastLogged = loadStats()['lastLogged']
+
+    Header("🌿 LifeLog Dashboard 🌿")
+    screen.print(f"Welcome back {username}!!")
+    screen.print(f"Last Login: {lastLogged} | Total Entires: {len(os.listdir(f".lifelog/{username}/"))}")
+    screen.print()
+    screen.print('-'*(os.get_terminal_size().columns-4))
+    screen.print("What would you like to do today?")
+    screen.print()
+    screen.print("[1] ✍️   Add a new entry")
+    screen.print("[2] 📖  View past entries")  
+    screen.print("[3] 🧹  Edit or delete an entry")  
+    screen.print("[4] 📊  View mood analytics")  
+    screen.print("[5] 🔒  Log out")  
+    screen.print()
+    screen.print('-'*(os.get_terminal_size().columns-4))
+    screen.print()
+    choice = int(screen.input("Enter your choice: "))
+    screen.render()
+    screen.clear()
+    if choice == 1:
+        AddEntry(username)
+    elif choice == 2:
+        ViewEntries(username)
+    elif choice == 3:
+        EditDelete(username)
+    elif choice == 4:
+        MoodAnalytics(username)
+    elif choice == 5:
+        Logout()
+        Quit(username, logged=True)
+    else:
+        screen.print("Invalid")
+        screen.render()
+        screen.clear()
+
+def Logout():
+    stats = loadStats()
+    stats['logged'] = False
+    with open(".lifelog/stats.dat", 'wb') as file:
+        pickle.dump(stats, file)
+
+def AddEntry(username: str):
+    Header("✍️  ADD A NEW ENTRY  ✍️")
+    screen.print(f"Date: {datetime.now().strftime("%d %B %Y")} | User: {username}")
+    title = screen.input("Title: ")
+    screen.print("-"*(os.get_terminal_size().columns - 4))
+    screen.print("Write your diary below (Type END on a new line to finish)")
+    diary = screen.editor("> ") 
+    screen.print()
+    screen.print("-"*(os.get_terminal_size().columns - 4))
+    screen.print()
+    screen.print("[1] 😊 Happy")
+    screen.print("[2] 😌 Calm")
+    screen.print("[3] 😔 Sad")
+    screen.print("[4] 😤 Stressed")
+    screen.print("[5] 😎 Confident")
+    mood = screen.input("Enter your mood (1-5):")
+    screen.print()
+
+    with open(f".lifelog/{username}/{title.replace(" ","_").lower()}.dat", 'wb') as file:
+        data = { "content": diary, "mood": mood, "date": datetime.now().strftime("%d %B %Y") }
+        pickle.dump(data, file)
+
+    screen.print("""Saving your entry...
+[✔] Entry saved successfully! 🌿
+“Remember, your thoughts are seeds — keep planting good ones.”""")
+    screen.print("-"*(os.get_terminal_size().columns - 4))
+    screen.input("Press enter to continue: ")
+    screen.clear()
+
+def CreateUser():
+    users = []
+
+    if os.path.exists(".lifelog/user.dat"):
+        with open(".lifelog/user.dat", 'rb') as F:
+            while True:
+                try:
+                    users.append(pickle.load(F)['username'])
+                except EOFError:
+                    break
+    Header("🌿 Create Account 🌿")
+    screen.render()
+    username = screen.input("Username (or q to quit): ")
+    if username in users:
+        screen.print("X Username already exists on this system. Try another name")
+        screen.render()
+        time.sleep(1)
+        screen.clear()
+        screen.render()
+        CreateUser()
+
+    else:
+        if username == 'q':
+            screen.quit()
+
+        password = screen.input("Password: ", True)
+        confirm = screen.input("Confirm Password: ", True)
+        if password != confirm:
+            screen.print("X Your password does not match. Try again")
+            screen.render()
+            time.sleep(1)
+            screen.clear()
+            screen.render()
+            CreateUser()
+        else:
+            usePass = screen.input("Do you want to protect your Diaries (remember your password) (Y/N): ")
+            if usePass.upper() not in ['Y', 'N']:
+                screen.print("Invalid answer. Expected Y or N. Try again!")
+                screen.render()
+                time.sleep(1)
+                screen.clear()
+                screen.render()
+                CreateUser()
+            else:
+                try:
+                    if not os.path.isdir(".lifelog/"):
+                        os.mkdir(".lifelog")
+                    with open('.lifelog/user.dat', 'ab') as file:
+                        pickle.dump({ "username": username, "password": password, "usePass":usePass}, file)
+                    with open(".lifelog/stats.dat", 'wb') as file:
+                        pickle.dump({"lastLogged": datetime.now().strftime("%d %B %Y"), "username":username}, file)
+                    if not os.path.isdir(f".lifelog/{username}"):
+                        os.mkdir(f".lifelog/{username}")
+
+                except Exception:
+                    exit()
+
+                screen.print("[✔] Account created successfully! 🌱")
+                screen.print("You can now log in to start your journal.")
+                screen.render()
+
+def Quit(username: str,logged: bool):
+    if logged:
+        with open(".lifelog/stats.dat", 'wb') as file:
+            pickle.dump({"lastLogged": datetime.now().strftime("%d %B %Y"), "username":username}, file)
+
+    screen.CenterText("═"*(os.get_terminal_size().columns - 6))
+    screen.print("🌿  BYE!! 🌿", True)
+    screen.print("Remember to log your thoughts tomorrow!!", True)
+    screen.print("═"*(os.get_terminal_size().columns - 10), True)
+    screen.render()
+    time.sleep(1)
+    screen.clear()
+
+
+def loadStats():
+    with open(".lifelog/stats.dat", 'rb') as stats:
+        return pickle.load(stats)
+try:
+# WELCOME SCREEN
+    screen = Window('LifeLog')
+    screen.print(logo,True)
+    screen.render()
+    time.sleep(2)
+    screen.clear()
+    screen.CenterText("═"*(os.get_terminal_size().columns - 6))
+    screen.print("🌿  WELCOME TO LIFELOG 🌿", True)
+    screen.print("═"*(os.get_terminal_size().columns - 10), True)
+    screen.print("Your personal digital diary & mood companion.", True)
+    screen.render()
+    time.sleep(2)
+    screen.clear()
+
+    if not os.path.isdir('.lifelog'):
+        screen.print("You seem new here!!!\nCreate an account to start your diary journey right now!!", True)
+
+    while True:
+        # DASHBOARD SCREEN
+        Header("🌿  WELCOME TO LIFELOG 🌿")
+        screen.print("""• [1] Login to your account  
+○ [2] Create a new account  
+○ [3] Exit""")
+        screen.render()
+        opt = ''
+        try:
+            opt = int(input(">>> "))
+        except Exception:
+            if opt == '':
+                opt = 1
+        screen.clear()
+        Header("🌿  WELCOME TO LIFELOG 🌿")
+        screen.print(f"""{'•' if opt == 1 else '○'} [1] Login to your account  
+{'•' if opt == 2 else '○'} [2] Create a new account  
+{'•' if opt == 3 else '○'} [3] Exit""")
+        screen.render()
+        time.sleep(0.5)
+        screen.clear()
+
+        if opt == 1:
+            if os.path.isdir('.lifelog'):
+                stats = loadStats()
+                try:
+                    if stats['logged']:
+                        Dashboard(stats['username'])
+                except KeyError:
+                    Login()
+            else:
+                Login() 
+
+        elif opt == 2:
+            CreateUser()
+
+        elif opt == 3:
+            Quit("", logged=False)
+            break
+
+        else:
+            screen.clear()
+            screen.render()
+        screen.clear()
+except KeyboardInterrupt:
+    exit()
---
+++ utils/window.py
@@ -0,0 +1,124 @@
+import os
+
+class Window():
+    def __init__(self, title: str="Window") -> None:
+        self.__clear = 'cls' if os.name == 'nt' else 'clear'
+        os.system(self.__clear)
+        self.__buffer = []
+        self.__pos = (0,0)
+        self.title = title
+        self.window_size = (
+            os.get_terminal_size().lines - 1,
+            os.get_terminal_size().columns,
+        )
+        self.__set_buffer()
+
+    def __remove_emoji(self, txt:str) -> int:
+        if any(element in txt for element in ['✍️', '📖', '🧹', '📊', '🔒', '🌿', '😊', '😌', '😔', '😤', '😎', '💭' ]):
+            return txt.count(txt[0]) + 1
+        else:
+            return 1
+
+    def __format(self, txt: str, centered: bool=False):
+        """
+        struc = list(" "*(self.window_size[1]))
+        txt = txt.strip()
+        struc[0] = struc[-1] = "|"
+        if centered:
+            for i in range(0, len(txt)):
+                struc[len(struc)//2 - len(txt)//2 + i] = txt[i]
+        else:
+            for i in range(0, len(txt)):
+                struc[2 + i] = txt[i]
+        struc = ''.join(struc)
+        return struc
+        """
+        partial = f"|{' '*((self.window_size[1] - len(txt))//2 - (self.__remove_emoji(txt)))}{txt}" if centered else f"| {txt}"
+        return f"{partial}{' '*(self.window_size[1] - len(partial)- (self.__remove_emoji(txt)))}|"
+
+    def __set_buffer(self):
+        self.__buffer.append(f"+{'-' * (self.window_size[1] - 2)}+")
+        for _ in range(self.window_size[0] - 2):
+            self.__buffer.append(self.__format(" "))
+        self.__buffer.append(f"+{'-' * (self.window_size[1] - 2)}+")
+        self.__pos = (1, 1)
+
+    def __add_slots(self, num: int):
+        currPos = self.__pos
+        self.__buffer[-1] = self.__format(" ")
+        for _ in range(0, num):
+            self.__buffer.append(self.__format(" "))
+        self.__buffer.append(f"+{'-' * (self.window_size[1] - 2)}+")
+        self.__pos = (currPos[0]-1, currPos[1])
+        self.render()
+
+    def clear(self) -> None:
+        os.system(self.__clear)
+        self.__buffer = []
+        self.__set_buffer()
+
+    def quit(self) -> None:
+        exit()
+
+    def render(self) -> None:
+        os.system(self.__clear)
+        for buf in self.__buffer:
+            print(buf)
+
+    def CenterText(self, msg: str):
+        self.__buffer[self.window_size[0]//2] = self.__format(msg, True)
+        self.__pos = (self.window_size[0]//2+1, self.__pos[1])
+
+    def truncate(self, text):
+        if len(text) <= self.window_size[1]-4:
+            return text
+
+        break_pos = text.rfind(' ', 0,  self.window_size[1]-4)
+        if break_pos == -1:
+            break_pos =  self.window_size[1]-4
+
+        line = text[:break_pos].rstrip()
+        remaining = text[break_pos:].lstrip()
+        return line + "\n" + self.truncate(remaining)
+
+
+    def print(self, msg: str=" ", centered: bool = False) -> None:
+        try:
+            for line in self.truncate(msg).split('\n'):
+                self.__buffer[self.__pos[0]] = self.__format(line, centered)
+                self.__pos = (self.__pos[0]+1, self.__pos[1])
+        except IndexError:
+            self.__add_slots(self.window_size[0])
+
+    def editor(self, msg: str, terminate: str="END") -> str:
+        self.print(msg)
+        self.render()
+        lines = []
+        try:
+            while True:
+                line = input("> ")
+                t = self.truncate(line).split("\n")
+                try:
+                    for i in range(0, len(t)):
+                        self.__buffer[self.__pos[0]-1 + i] = self.__format(msg + t[i])
+                        self.render()
+                except IndexError:
+                    self.__add_slots(20)
+                self.__pos = (self.__pos[0] + len(t), self.__pos[1])
+                lines.append(line)
+                if line == terminate:
+                    break
+        except EOFError:
+            return '\n'.join(lines)
+        return '\n'.join(lines)
+
+    def input(self, msg: str, hidden: bool = False) -> str:
+        self.print(msg)
+        self.render()
+        result = input(">>> ")
+        if not hidden:
+            self.__buffer[self.__pos[0]-1] = self.__format(msg + result)
+        else:
+            self.__buffer[self.__pos[0]-1] = self.__format(msg + ('*'*len(result)))
+        self.render()
+        return result
